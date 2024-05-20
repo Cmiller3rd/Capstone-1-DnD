c37d11b3e9136419b41381c699e670a75be410cc
Carl Miller <cmiller3rd@Carls-Air.lan>
2024-05-20 07:37:22 -0500

    first commit

---
+++ .ipynb_checkpoints/sbux_data-checkpoint.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib as plt
+import csv
+import numpy as np
+
+promo_data = pd.read_csv("../Capstone_I/transcript.csv")
+promo_summary = pd.read_csv('../Capstone_I/profile.csv')
+promo_folio = pd.read_csv('../Capstone_I/portfolio.csv')
+print(promo_data)
+print(promo_summary)
+print(promo_folio)
